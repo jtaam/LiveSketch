4415fc00f31a621382fc6161094f895366cd5c4a
dev-tam <tamjid.mdh@gmail.com>
2018-01-23 04:42:51 +0600

    first sketching

---
+++ LiveSketching.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+# Our sketch generation function
+def sketch(image):
+    # Convert image to grayscale
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Clean up image using Gaussian Blur
+    img_gray_blur = cv2.GaussianBlur(img_gray,(5,5),0)
+    
+    # Extract edges
+    canny_edges = cv2.Canny(img_gray_blur,10,70)
+    
+    # Do an invert binarize the image
+    ret, mask = cv2.threshold(canny_edges, 70,255, cv2.THRESH_BINARY_INV)
+    return mask
+
+# Webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("Our Live Sketcher", sketch(frame))
+    if cv2.waitKey(1)==13: #13 is the Enter key
+        break
+        
+# Release camera and close windows
+cap.release()
+cv2.destroyAllWindows()
+
